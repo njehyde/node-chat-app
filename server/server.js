9108e89cc25df363d2a4ca937a061b44b69d74d2
Nicholas Hyde <njehyde@gmail.com>
2018-03-18 19:11:37 +0000

    Add geolocation support via geolocation api

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -22,11 +22,13 @@ io.on('connection', (socket) => {
 
     socket.on('createMessage', (message, callback) => {
         console.log('createMessage', message);
-
         io.emit('newMessage', generateMessage(message.from, message.text));
-
         callback('This is from the server');
-        // socket.broadcast.emit('newMessage', generateMessage(message.from, message.text));
+    });
+
+    socket.on('createLocationMessage', (coords) => {
+        console.log(coords);
+        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
     });
 
     socket.on('disconnect', () => {
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
     it('should generate the correct message object', () => {
@@ -9,6 +9,18 @@ describe('generateMessage', () => {
         var res = generateMessage(from, text);
         expect(res.from).toBe(from);
         expect(res.text).toBe(text);
+        expect(typeof res.createdAt).toBe('number');
+    });
+});
+
+describe('generateLocationMessage', () => {
+    it('should generate the correct location object', () => {
+        var from = 'Test';
+        var latitude = 50;
+        var longitude = 40;
+        var res = generateLocationMessage(from, latitude, longitude);
+        expect(res.from).toBe(from);
+        expect(res.url).toBe(`https://www.google.com/maps?q=${latitude},${longitude}`);
         expect(typeof res.createdAt).toBe('number');
     });
 });
